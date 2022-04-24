ea9a90afbed693f2c4a1a0948a4ccba7d774ef2b
Edivan Sousa <94314273+sousaep1974@users.noreply.github.com>
2022-04-23 21:18:17 -0300

    Update

---
+++ main.js
@@ -0,0 +1,6 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./function.mjs";
+
+console.log(mostraIdade("Edivan", 48));
+console.log(mostraCidade("Edivan", "Parnamirim"));
+console.log(mostraHobby("Edivan", "Estudar"));  
+
