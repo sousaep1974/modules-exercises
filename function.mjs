fc15e18e03ee70851340c3737667323eda3e2e93
Edivan Sousa <94314273+sousaep1974@users.noreply.github.com>
2022-04-24 02:09:54 -0300

    Alinhamento do código

--- function.mjs
+++ function.mjs
@@ -1,13 +1,13 @@
 function mostraIdade(nome, idade) {
-  return `A idade de ${nome} é ${idade}`;
+return `A idade de ${nome} é ${idade}`;
 }
 
 function mostraCidade(nome, cidade) {
-  return `A cidade de ${nome} é ${cidade}`;
+return `A cidade de ${nome} é ${cidade}`;
 }
 
 function mostraHobby(nome, hobby) {
-  return `O hobby de ${nome} é ${hobby}`;
+return `O hobby de ${nome} é ${hobby}`;
 }
 
 export { mostraIdade, mostraCidade, mostraHobby };
--- main.js
+++ main.js
@@ -2,5 +2,4 @@ import { mostraIdade, mostraCidade, mostraHobby } from "./function.mjs";
 
 console.log(mostraIdade("Edivan", 48));
 console.log(mostraCidade("Edivan", "Parnamirim"));
-console.log(mostraHobby("Edivan", "Estudar"));  
-
+console.log(mostraHobby("Edivan", "Estudar"));
